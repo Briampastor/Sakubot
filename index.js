de75587b6ca04f5d22fc44acf7a5db748ba86b18
Briampastor <96449359+Briampastor@users.noreply.github.com>
2021-12-21 13:27:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,60 @@
-const { WAConnection } = require('@adiwajshing/baileys');
-const fs = require('fs');
+console.log('Starting...')
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('Lightweight\nWhatsApp Bot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
 
-async function iniciar () { 
-        const client = new WAConnection()
+var isRunning = false
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+  // console.log(p)
+}
 
-        client.logger.level = 'warn'
-
-        client.on('qr', () => {
-        })
-
-        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
-
-        client.on('connecting', () => {
-        console.log('Conectando')
-        })
-
-        client.on('open', () => {
-        console.log('Conectado exitosamente :D')
-        })
-        await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        }
-
-iniciar (npm start)
-.catch (err => console.log("unexpected error: " + err))
+start('main.js')
